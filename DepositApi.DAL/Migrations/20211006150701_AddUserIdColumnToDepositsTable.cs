88df7aa1f09bc60f11249371bda93ec274801558
Danylo Kolohryvko <yoodenkol@gmail.com>
2021-10-06 18:43:21 +0300

    Added Db migration

---
+++ DepositApi.DAL/Migrations/20211006150701_AddUserIdColumnToDepositsTable.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DepositApi.DAL.Migrations
+{
+    public partial class AddUserIdColumnToDepositsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Deposits",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Deposits");
+        }
+    }
+}
